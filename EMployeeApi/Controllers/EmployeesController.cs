5201391c3c78e7a1fb5b4427d6cd890c4615c741
Biswajit <biswajitpatro888outlook.com>
2020-04-09 17:33:01 +0530

    Initial commit

---
+++ EMployeeApi/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("CorsPolicy")]
+    public class EmployeesController : ControllerBase
+    {
+        static List<Employee> emps = new List<Employee>()
+        {
+            new Employee(){Eid=1,Name="Biswajit",sal=34000,Designation="Devloper"},
+             new Employee(){Eid=2,Name="Subrat",sal=30000,Designation="coder"},
+              new Employee(){Eid=3,Name="Dev",sal=54000,Designation="Analyst"},
+               new Employee(){Eid=4,Name="Devid",sal=3000,Designation="tester"},
+                new Employee(){Eid=5,Name="Kk",sal=34000,Designation="Devloper"},
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return emps;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            return emps[id];
+        }
+    }
+}
---
+++ EMployeeApi/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMployeeApi
+{
+    public class Employee
+    {
+        public int Eid{ get; set; }
+        public string Name { get; set; }
+        public float sal { get; set; }
+        public string Designation { get; set; }
+    }
+}
